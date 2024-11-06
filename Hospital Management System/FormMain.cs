6782f3dbfbf0e713cd9633886c68affc53ea4d84
fakhri-git <test@fakhri.com>
2024-11-06 18:54:11 +0700

    remove hide form main

--- Hospital Management System/FormMain.cs
+++ Hospital Management System/FormMain.cs
@@ -33,43 +33,36 @@ namespace Hospital_Management_System
         private void btnICD_Click(object sender, EventArgs e)
         {
             new FormMaster_ICD_11().Show();
-            Hide();
         }
 
         private void btnDoctor_Click(object sender, EventArgs e)
         {
             new FormMasterDoctor(null).Show();
-            Hide();
         }
 
         private void btnParent_Click(object sender, EventArgs e)
         {
             new FormMasterPatient().Show();
-            Hide();
         }
 
         private void btnNewMeeting_Click(object sender, EventArgs e)
         {
             new FormNewMeeting().Show();
-            Hide();
         }
 
         private void btnManageMeeting_Click(object sender, EventArgs e)
         {
             new FormManageMeeting().Show();
-            Hide();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             new FormPayment().Show();
-            Hide();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             new FormNewPayment().Show();
-            Hide();
         }
     }
 }
